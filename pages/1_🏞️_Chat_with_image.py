c99839cf5f2086acc44d5d281cfe0525e0894795
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-07-26 15:12:02 +0530

    Update 1_🏞️_Chat_with_image.py

--- pages/1_🏞️_Chat_with_image.py
+++ pages/1_🏞️_Chat_with_image.py
@@ -8,6 +8,7 @@ from langchain.chains import LLMChain
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.chat_models import ChatOpenAI
 from io import BytesIO
+import openai
 
 openai.api_key = st.secrets["chat_gpt_key"]
 
